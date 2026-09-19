2582af2fe5e143cdf5689293d36c8639cea261a5
onoja ojonugba monday <onojamondayojonugba@gmail.com>
2026-09-19 17:25:59 +0100

    fix(migrations): add academic migration versions through 10

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -99,6 +99,36 @@ func Migrate(db *gorm.DB) error {
         },
     }
 
+        {
+            Version: 5,
+            Name:    "subjects",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.Subject{}) },
+        },
+        {
+            Version: 6,
+            Name:    "teacher_assignments",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.TeacherAssignment{}) },
+        },
+        {
+            Version: 7,
+            Name:    "student_enrollment",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.StudentEnrollment{}) },
+        },
+        {
+            Version: 8,
+            Name:    "attendance",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.Attendance{}) },
+        },
+        {
+            Version: 9,
+            Name:    "assessments",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.Assessment{}) },
+        },
+        {
+            Version: 10,
+            Name:    "assessment_results",
+            Up: func(tx *gorm.DB) error { return tx.AutoMigrate(&models.AssessmentResult{}) },
+        },
     for _, migration := range migrations {
         var applied Migration
         result := db.Where("version = ?", migration.Version).First(&applied)

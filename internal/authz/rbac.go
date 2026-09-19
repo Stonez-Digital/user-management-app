321902e6f9349093a22749abea72ccd2a11ddd57
onoja ojonugba monday <onojamondayojonugba@gmail.com>
2026-09-19 13:24:33 +0100

    feat(student): add student permissions

--- internal/authz/rbac.go
+++ internal/authz/rbac.go
@@ -19,13 +19,19 @@ const (
     PermissionUsersDeactivate = "users.deactivate"
     PermissionRolesAssign = "roles.assign"
     PermissionAuditRead = "audit.read"
+    PermissionStudentsRead = "students.read"
+    PermissionStudentsCreate = "students.create"
+    PermissionStudentsUpdate = "students.update"
+    PermissionStudentsDelete = "students.delete"
 )
 
 var rolePermissions = map[string]map[string]bool{
     RoleSuperAdmin: {
         PermissionUsersRead: true, PermissionUsersCreate: true, PermissionUsersUpdate: true,
         PermissionUsersDelete: true, PermissionUsersActivate: true, PermissionUsersDeactivate: true,
+        PermissionStudentsRead: true, PermissionStudentsCreate: true, PermissionStudentsUpdate: true, PermissionStudentsDelete: true,
         PermissionRolesAssign: true, PermissionAuditRead: true,
+        PermissionStudentsRead: true, PermissionStudentsCreate: true, PermissionStudentsUpdate: true, PermissionStudentsDelete: true,
     },
     RoleSchoolAdmin: {
         PermissionUsersRead: true, PermissionUsersCreate: true, PermissionUsersUpdate: true,

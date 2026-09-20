cc69495539088486366ccfe09ef02aa183d9aeeb
onoja ojonugba monday <onojamondayojonugba@gmail.com>
2026-09-20 18:51:22 +0100

    security(auth): store refresh token hashes

--- internal/models/refresh_token.go
+++ internal/models/refresh_token.go
@@ -9,7 +9,7 @@ import (
 type RefreshToken struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
 	UserID    uuid.UUID `gorm:"type:uuid;index"`
-	Token     string    `gorm:"uniqueIndex"`
+	TokenHash string    `gorm:"uniqueIndex;size:64"`
 	ExpiresAt time.Time
 	Revoked   bool      `gorm:"default:false"`
 	CreatedAt time.Time

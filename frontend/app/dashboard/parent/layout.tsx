0b9d14a758a5df518db089da27753df7f11acf3d
onoja ojonugba monday <onojamondayojonugba@gmail.com>
2026-09-24 12:14:02 +0100

    refactor: scope academic context to role workflows

---
+++ frontend/app/dashboard/parent/layout.tsx
@@ -0,0 +1,5 @@
+"use client";
+import { ReactNode } from "react";
+import { AcademicContextProvider } from "../../../lib/academic-context";
+import AcademicSelector from "../../../components/academic-selector";
+export default function ParentDashboardLayout({children}:{children:ReactNode}){return <AcademicContextProvider><AcademicSelector/>{children}</AcademicContextProvider>;}
